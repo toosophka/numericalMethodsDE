03fa2aa8411acd19a1ad9ce6a1dd2a942491be97
Sophia Yakovleva <63459167+toosophka@users.noreply.github.com>
2020-05-19 17:39:03 +0700

    Add files via upload

---
+++ vchmat6/vchmat6/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace vchmat6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series11 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series12 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series13 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series14 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series15 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea3.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea3);
+            legend3.Name = "Legend1";
+            this.chart1.Legends.Add(legend3);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series11.BorderWidth = 3;
+            series11.ChartArea = "ChartArea1";
+            series11.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series11.Legend = "Legend1";
+            series11.Name = "Метод Эйлера";
+            series11.YValuesPerPoint = 2;
+            series12.BorderWidth = 3;
+            series12.ChartArea = "ChartArea1";
+            series12.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series12.Color = System.Drawing.Color.Red;
+            series12.Legend = "Legend1";
+            series12.Name = "Точное решение";
+            series13.BorderWidth = 3;
+            series13.ChartArea = "ChartArea1";
+            series13.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series13.Color = System.Drawing.Color.Green;
+            series13.Legend = "Legend1";
+            series13.Name = "Модифицированный метод Эйлера";
+            series14.BorderWidth = 3;
+            series14.ChartArea = "ChartArea1";
+            series14.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series14.Color = System.Drawing.Color.Fuchsia;
+            series14.LabelForeColor = System.Drawing.Color.BlanchedAlmond;
+            series14.Legend = "Legend1";
+            series14.MarkerBorderColor = System.Drawing.Color.Fuchsia;
+            series14.MarkerBorderWidth = 5;
+            series14.MarkerColor = System.Drawing.Color.Fuchsia;
+            series14.MarkerStyle = System.Windows.Forms.DataVisualization.Charting.MarkerStyle.Circle;
+            series14.Name = "Метод Рунге-Кутты Мерсона";
+            series14.YValuesPerPoint = 2;
+            series15.BorderWidth = 3;
+            series15.ChartArea = "ChartArea1";
+            series15.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series15.Color = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            series15.Legend = "Legend1";
+            series15.Name = "Метод Адамса 3го порядка";
+            this.chart1.Series.Add(series11);
+            this.chart1.Series.Add(series12);
+            this.chart1.Series.Add(series13);
+            this.chart1.Series.Add(series14);
+            this.chart1.Series.Add(series15);
+            this.chart1.Size = new System.Drawing.Size(651, 405);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(669, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(202, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Введите координату х конечной точки:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(672, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(666, 52);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(130, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "Введите значение шага:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(669, 68);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(889, 68);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 14;
+            // 
+            // label9
+            // 
+            this.label9.Location = new System.Drawing.Point(889, 12);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(100, 53);
+            this.label9.TabIndex = 13;
+            this.label9.Text = "Для метода Рунге Кутты Мерсона введите точность:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(669, 117);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 23);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Метод Эйлера";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(669, 155);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(100, 49);
+            this.button2.TabIndex = 20;
+            this.button2.Text = "Метод Эйлера (модифицированный)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(775, 117);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Удалить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(775, 181);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 22;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(669, 221);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 54);
+            this.button5.TabIndex = 23;
+            this.button5.Text = "Метод Рунге-Кутты Мерсона";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(775, 252);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 26;
+            this.button6.Text = "Удалить";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(669, 297);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 41);
+            this.button7.TabIndex = 27;
+            this.button7.Text = "Метод Адамса 3го порядка";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(775, 315);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 28;
+            this.button8.Text = "Удалить";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(669, 363);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(100, 36);
+            this.button9.TabIndex = 29;
+            this.button9.Text = "Точное решение";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(775, 376);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 30;
+            this.button10.Text = "Удалить";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 494);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ vchmat6/vchmat6/Form1.cs
@@ -0,0 +1,239 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace vchmat6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        double x0 = 0, y0 = 10, xn;
+        int n;
+
+        double[] x;
+        double[] y;
+
+        //проверка на пустоту
+        private bool empty(TextBox box)
+        {
+            if (box.Text == "")
+                return true;
+            else
+                return false;
+        }
+
+        private bool check()
+        {
+            if(empty(textBox1))
+            {
+                MessageBox.Show("Введите координату х конечной точки.");
+                return false;
+            }
+            if (empty(textBox2))
+            {
+                MessageBox.Show("Введите шаг.");
+                return false;
+            }
+            if((double.Parse(textBox2.Text)<0)||(double.Parse(textBox2.Text)> double.Parse(textBox1.Text)))
+            {
+                MessageBox.Show("Шаг введен некорректно.");
+                textBox2.Clear();
+                return false;
+            }
+            return true;
+        }
+
+        //функция, подготавливающая данные для использования методов, задаются значения переменных
+        private void data(double h)
+        {
+            xn = double.Parse(textBox1.Text);       //координата х конечной точки
+            n = (int)Math.Ceiling((xn - x0) / h);    //количество интервалов, округляется в бОльшую
+            x = new double[n + 1];                       //сторону, если (x(n)-x(0)) не делится на h
+            y = new double[n + 1];
+            
+            //заполняем массив х в соответствии с шагом
+            for (int i = 0; i < n; i++)
+                x[i] = x0 + h * i;
+            x[n] = xn;
+        }
+
+        //построение графика, настраиваются оси
+        private void graph(double[] x, double[] y, double h, int i)
+        {
+            chart1.ChartAreas[0].AxisX.Minimum = x0-3; //вычитаем произвольное число, чтобы на графике было лучше видно
+            chart1.ChartAreas[0].AxisX.Maximum = double.Parse(textBox1.Text)+3; //аналогично мин
+            chart1.ChartAreas[0].AxisX.MajorGrid.Interval = h;
+            chart1.Series[i].Points.DataBindXY(x, y);
+        }
+
+        //вычисляет функцию f(x,y)
+        double function(double x, double y)
+        {
+            return (Math.Sin(x) - y);
+        }
+
+        //стереть график метод Эйлера
+        private void button3_Click(object sender, EventArgs e)
+        {
+            chart1.Series[0].Points.Clear();
+        }
+
+        //стереть график модифицированный
+        private void button4_Click(object sender, EventArgs e)
+        {
+            chart1.Series[2].Points.Clear();
+        }
+
+        //метод Рунге-Кутты Мерсона
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!check())
+                return;
+            if(empty(textBox5))
+            {
+                MessageBox.Show("Введите точность.");
+                return;
+            }
+            chart1.Series[3].Points.Clear();
+            double h = double.Parse(textBox2.Text);
+            xn = double.Parse(textBox1.Text);       //координата х конечной точки
+            double eps= double.Parse(textBox5.Text);
+            double[] k = new double[5];
+            double delta;
+            double x = x0, y = y0;
+
+            chart1.Series[3].Points.AddXY(x, y);
+            while (x<xn)
+            {
+                k[0] = h * function(x, y);
+                k[1] = h * function(x + (double)1 / 3 * h, y + (double)1 / 3 * k[0]);
+                k[2] = h * function(x + (double)1 / 3 * h, y + (double)1 / 6 * k[0] + (double)1 / 6 * k[1]);
+                k[3] = h * function(x + (double)1 / 2 * h, y + (double)1 / 8 * k[0] + (double)3 / 8 * k[2]);
+                k[4] = h * function(x + h, y + (double)1 / 2 * k[0] - (double)3 / 2 * k[2] + 2 * k[3]);
+
+                delta = (double)1 / 30 * (2 * k[0] - 9 * k[2] + 8 * k[3] - k[4]);
+                if (Math.Abs(delta) >= eps*Math.Abs(y))
+                {
+                    h = (double)h / 2;
+                    continue;
+                }
+                chart1.Series[3].Points.AddXY(x, y);
+                y += (double)1 / 6 * k[0] + (double)2 / 3 * k[3] + (double)1 / 6 * k[4];
+                x += h;
+                if (Math.Abs(delta) <= (double)eps * Math.Abs(y) / 32)
+                    h = (double)h * 2;
+            }
+            k[0] = h * function(xn, y);
+            k[1] = h * function(xn + (double)1 / 3 * h, y + (double)1 / 3 * k[0]);
+            k[2] = h * function(xn + (double)1 / 3 * h, y + (double)1 / 6 * k[0] + (double)1 / 6 * k[1]);
+            k[3] = h * function(xn + (double)1 / 2 * h, y + (double)1 / 8 * k[0] + (double)3 / 8 * k[2]);
+            k[4] = h * function(xn + h, y + (double)1 / 2 * k[0] - (double)3 / 2 * k[2] + 2 * k[3]);
+            y += (double)1 / 6 * k[0] + (double)2 / 3 * k[3] + (double)1 / 6 * k[4];
+            chart1.Series[3].Points.AddXY(xn, y);
+        }
+
+        //стереть график Рунге-Кутты Мерсона
+        private void button6_Click(object sender, EventArgs e)
+        {
+            chart1.Series[3].Points.Clear();
+        }
+
+        //точное решение
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (!check())
+                return;
+            double h = double.Parse(textBox2.Text);
+            data(h);
+            y[0] = y0;
+            for (int i = 1; i < n + 1; i++)
+                y[i] = -0.5 * Math.Cos(x[i]) + 0.5 * Math.Sin(x[i]) + (double)(21 / 2) * Math.Exp(-x[i]);
+
+            graph(x, y, h, 1);
+        }
+
+        //стереть точное решение
+        private void button10_Click(object sender, EventArgs e)
+        {
+            chart1.Series[1].Points.Clear();
+        }
+
+        //метод Адамса 3го порядка
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (!check())
+                return;
+            double h = double.Parse(textBox2.Text);
+            data(h);
+            double[] k = new double[3];
+            double delta;
+
+            y[0] = y0;
+            y[1] = eiler(h, x[0], y[0]);
+            y[2] = eiler(h, x[1], y[1]);
+
+            for(int i=3;i<n;i++)
+            {
+                for (int j = 0; j < 3; j++)
+                    k[j] = function(x[i - 1], y[i - 1])*h;
+                delta = (double)1 / 12 * (23 * k[0] - 16 * k[1] + 5 * k[2]);
+                y[i] = y[i - 1] + delta;
+            }
+            graph(x, y, h, 4);
+        }
+
+        double eiler(double h, double x, double y)
+        {
+            return ( y + h * function(x + h / 2, y + h / 2 * function(x, y)));
+        }
+
+        //стереть график Адамса
+        private void button8_Click(object sender, EventArgs e)
+        {
+            chart1.Series[4].Points.Clear();
+        }
+
+        //метод Эйлера модифицированный
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!check())
+                return;
+            double h = double.Parse(textBox2.Text);
+            data(h);
+
+            y[0] = y0;
+            for (int i = 1; i < n; i++)
+                y[i] = eiler(h, x[i - 1], y[i - 1]); 
+            y[n] = eiler(h, x[n - 1], y[n - 1]);
+
+            graph(x, y, h, 2);
+        }
+        
+        //метод Эйлера
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!check())
+                return;
+            double h = double.Parse(textBox2.Text);
+            data(h);
+            double h1 = x[n] - x[n - 1];
+
+            y[0] = y0;
+            for (int i = 1; i < n; i++)
+                y[i] = y[i - 1] + h * function(x[i - 1], y[i - 1]);
+            y[n]= y[n - 1] + h1 * function(x[n - 1], y[n - 1]);
+            graph(x, y, h, 0);
+        }
+    }
+}
